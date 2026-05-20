e89d2ab0edd41096b7371830a903cba963bf84d8
Saif-ur-Rehman Awan <saifurrehmanawan85@gmail.com>
2026-05-20 22:35:26 +0500

    preemtive sjf added to sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -42,7 +42,8 @@ void process_input(std::vector<process> &processes){
 }
 
 void display_table_on_terminal(const std::vector<process> processes){
-    std::cout<<"Data Stored:\n\n";
+    std::cout<<"Data Given:\n";
+    std::cout<<"------------------------------------------------------\n";
     std::cout<<"pID\t|\tAT\t|\tBT\t|\tP\n";
     std::cout<<"------------------------------------------------------\n";
     for(const process &p : processes){
@@ -57,9 +58,9 @@ void display_table_on_terminal(const std::vector<process> processes){
 
 
 
-void display_logs_on_terminal(const std::vector<process> processes){
+// void display_logs_on_terminal(const std::vector<process> processes){
 
-}
+// }
 
 void fill_readyqueue_non_preemtive_fcfs(std::vector<process> processes, std::queue<int> &readyqueue){        // to put processes in Ready Queue in FCFS
     // int n=processes.size();
@@ -83,79 +84,123 @@ void fill_readyqueue_non_preemtive_fcfs(std::vector<process> processes, std::que
     
 }
 
-void fill_readyqueue_non_preemtive_sjf(std::vector<process> &processes, std::queue<int> &readyqueue){         // to fill readyqueue based on non preemtive SJF
-    // int shortJobID{};
-    // bool isshort{false};
-    
-    for(int j{0};j<processes.size();j++){
+void fill_readyqueue_non_preemtive_sjf(std::vector<process> &processes, std::queue<int> &readyqueue){   // to fill readyqueue for non-preemtive sjf
 
+    int completed = 0;
+    int currentTime = 0;
+
+    while (completed < processes.size()) {
+
+        // store AVAILABLE process indices
         std::vector<int> available_processes;
-        
-        for(int i{0};i<processes.size();i++){
 
-            int n=processes.size();
-            // bool isShort[n];
-            bool isShort{false};
-            // for(bool x: isShort){
-            //     x=false;
-            // }
+        // find all arrived and unfinished processes
+        for (int i = 0; i < processes.size(); i++) {
 
-            bool isAvailable{false};
-            if(!available_processes.empty()){
-                auto it=std::find(available_processes.begin(), available_processes.end(), processes[i].pID);
-                if(it!= available_processes.end()){
-                    isAvailable=true;
-                }else{
-                    isAvailable=false;
-                }
-            }
-
-            if(processes[i].AT<=currentTime){
-                if(!isAvailable){
-                    available_processes.push_back(processes[i].pID);
-                    // available_processes.push_back(i);
-                }
-                for(int x: available_processes){
-                    if(!processes[x].in_queue){
-                        auto it=std::min_element(available_processes.begin(), available_processes.end(),
-                        [&](int a, int b){
-                            return processes[a].BT<processes[b].BT;
-                        }
-                    );
-                    int shortestIdx=*it;
-                    if(shortestIdx==i){
-                        isShort=true;
-                    }
-                    // isShort=*it;
-                        // if(*it==processes[i].pID){
-                        //     isShort=true;
-                        // }
-                    }
-                }
-                
-
-                if(!processes[i].in_queue && isShort){
-                    readyqueue.push(processes[i].pID);
-                    processes[i].in_queue=true;
-                }
-                
+            if (processes[i].AT <= currentTime &&
+                !processes[i].in_queue)
+            {
+                available_processes.push_back(i);
             }
         }
-        currentTime++;
+
+        // if CPU is idle
+        if (available_processes.empty()) {
+
+            currentTime++;
+            continue;
+        }
+
+        // find shortest burst time process
+        auto shortest = std::min_element(
+
+            available_processes.begin(),
+            available_processes.end(),
+
+            [&](int a, int b) {
+
+                return processes[a].BT <
+                       processes[b].BT;
+            }
+        );
+
+        int idx = *shortest;
+
+        // push INDEX into queue
+        readyqueue.push(idx);
+
+        processes[idx].in_queue = true;
+
+        // simulate execution
+        currentTime += processes[idx].BT;
+
+        completed++;
     }
 }
 
-// void calculations_fcfs_nonpreemtive(std::vector<process> &processes, std::queue<int> readyqueue){  // to calculate the remaining data from CT
-//     while(!readyqueue.empty()){
-//         int idx=readyqueue.front();
-//         processes[idx].TT=processes[idx].CT-processes[idx].AT;
-//         processes[idx].WT=processes[idx].TT-processes[idx].BT;
-//         processes[idx].RT=processes[idx].assignTime-processes[idx].AT;
+void initialize_processes_preemtive_sjf(std::vector<process> &processes)
+{
+    for (process &p : processes) {
 
-//         readyqueue.pop();
-//     }
+        p.lBT = p.BT;
+    }
+}
 
-// }
+void execute_preemptive_sjf(std::vector<process> &processes)
+{
+    int completed = 0;
+    int currentTime = 0;
+
+    while (completed < processes.size()) {
+
+        int shortest = -1;
+
+        // Find shortest remaining job
+        for (int i = 0; i < processes.size(); i++) {
+
+            if (processes[i].AT <= currentTime &&
+                processes[i].lBT > 0)
+            {
+                if (shortest == -1 ||
+                    processes[i].lBT <
+                    processes[shortest].lBT)
+                {
+                    shortest = i;
+                }
+            }
+        }
+
+        // CPU idle
+        if (shortest == -1) {
+
+            currentTime++;
+            continue;
+        }
+
+        // First response time
+        if (processes[shortest].lBT ==
+            processes[shortest].BT)
+        {
+            processes[shortest].assignTime =
+                currentTime;
+        }
+
+        // Execute for 1 unit
+        processes[shortest].lBT--;
+
+        currentTime++;
+
+        // Process finished
+        if (processes[shortest].lBT == 0) {
+
+            processes[shortest].CT =
+                currentTime;
+
+            completed++;
+        }
+    }
+}
+
 
 void calculate_TT_WT_RT(std::vector<process> &processes){  // improved func to calculate the remaining data of the process from CT
     for(process &p:processes){
@@ -193,6 +238,7 @@ void display_readyqueue(const std::vector<process> processes, std::queue<int> re
 }
 
 void display_final_result(const std::vector<process> processes, const std::queue<int> readyqueue){          // to show the final output table
+    std::cout<<"\n---------------------------------------------------------------------------------------------------------------------------------------------------\n";
     std::cout<<"pID\t|\tAT\t|\tBT\t|\tP\t|\tWT\t|\tTT\t|\tRT\t|\tCT\n";
     std::cout<<"---------------------------------------------------------------------------------------------------------------------------------------------------\n";
     for(const process p : processes){
@@ -207,13 +253,13 @@ void display_final_result(const std::vector<process> processes, const std::queue
                 <<'\n';
         
     }
-    std::cout<<"---------------------------------------------------------------------------------------------------------------------------------------------------\n";
+    std::cout<<"---------------------------------------------------------------------------------------------------------------------------------------------------\n\n";
 
 
 }
 
 int main(){
-    std::cout<<"hello\n";
+    
 
     std::vector<process> processes;
     std::queue<int> readyqueue;
@@ -223,21 +269,32 @@ int main(){
 
     display_table_on_terminal(processes);           // to display the initial values inputed
 
+
+    // for fcfs
     // fill_readyqueue_non_preemtive_fcfs(processes, readyqueue);       // to fill process into readyqueue in fcfs
 
-    fill_readyqueue_non_preemtive_sjf(processes,readyqueue);        // to fill readyqueue based on non preemtive SFJ
 
-    display_readyqueue(processes,readyqueue);               // to see the readyqueue data that is being stored in it.
+    // for non preemtive sjf
+    // fill_readyqueue_non_preemtive_sjf(processes,readyqueue);        // to fill readyqueue based on non preemtive SFJ
+
+
+
+    // display_readyqueue(processes,readyqueue);               // to see the readyqueue data that is being stored in it.
+
+
 
     // display_logs_on_terminal(processes);
 
     
+    // for fcfs
+    // executing_readyqueue_fcfs(processes, readyqueue);           // to calculate CT of each process
 
-    executing_readyqueue_fcfs(processes, readyqueue);           // to calculate CT of each process
 
-    // calculations_fcfs_nonpreemtive(processes,readyqueue);    // to calculate remaining data from CT, better to use the improved version  
-
+    // for preemtive sjf
+    initialize_processes_preemtive_sjf(processes);      // to initialize lBT(left brust time variable) for preemtive sjf
     
+    execute_preemptive_sjf(processes);                  // to execute processes in preemtive sjf (does not use readyqueue)
+
 
 
 
